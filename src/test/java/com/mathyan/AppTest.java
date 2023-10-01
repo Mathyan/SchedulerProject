c742831be5f4f3fbe878edd4c32e937c8a62e5a9
Mathyan <matijan.cvitanovic@outlook.com>
2023-10-01 15:35:02 +0200

    init

---
+++ src/test/java/com/mathyan/AppTest.java
@@ -0,0 +1,20 @@
+package com.mathyan;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
